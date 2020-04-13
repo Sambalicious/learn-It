d490a2bb556b535871abc5b3d85814f7818ba34e
Sambalicious <stemitope370@gmail.com>
2020-04-13 15:05:23 +0100

    added more

--- src/components/DashBoardComponent/AvatarComp.jsx
+++ src/components/DashBoardComponent/AvatarComp.jsx
@@ -16,12 +16,12 @@ const AvatarComp = ({status}) => {
     return ( 
         
        <div>
-         { state.authDetails ?  <div class=" rounded p-6 bg-indigo-500">
-            <img class="h-16 w-16 rounded-full mx-auto" alt="profile_image" src={imageUrl} />
-            <div class="text-center">
-              <h2 class="text-lg font-bold mb-2 uppercase text-white">{name} </h2>
-              <div class="text-red-400 font-bold">{status} </div>
-              <div class="text-white font-bold"> {email} </div>
+         { state.authDetails ?  <div className=" rounded p-6 bg-indigo-500">
+            <img className="h-16 w-16 rounded-full mx-auto" alt="profile_image" src={imageUrl} />
+            <div className="text-center">
+              <h2 className="text-lg font-bold mb-2 uppercase text-white">{name} </h2>
+              <div className="text-red-400 font-bold">{status} </div>
+              <div className="text-white font-bold"> {email} </div>
              
            </div>
         </div> : null}
--- src/components/DashBoardComponent/Form.jsx
+++ src/components/DashBoardComponent/Form.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useContext, useEffect } from 'react';
-import {GlobalContext} from '../../App';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
+import InputField from './InputField';
+import Button from './Button';
 
 
 
 const Form = () => {
 
-      const {handleAddContent} = useContext(GlobalContext) ;
         const [title, setTitle]= useState('');
         const [description, setDescription] = useState('');
         const [video, setVideo] = useState('');
         const [image,setImage] =useState('');
-        const [author, setAuthor] = useState('')
+        const [author, setAuthor] = useState('');
+        
     
         const handleTitle = (e) => {
           setTitle(e.target.value)
@@ -33,18 +34,24 @@ const Form = () => {
           setAuthor(e.target.value)
         }
 
-        useEffect(()=>{
+        const [imageUrl, setImageUrl] = useState('');
+        const [imageError, setImageError] = useState('');
 
-          axios.post("https://api.imgur.com/3/image", image, {
+        useEffect(()=>{            
+          axios.post("https://api.imgur.com/3/image",image, {
                         headers:{
-                          Authorization: "client-ID 890290167c69fc4"
-                        },
-                      }).then(response=>console.log(response.data))
-                      .then(error => console.log (error))
+                          Authorization: "Client-ID db563de1e18b82b"
+                        }
+                      }).then(response=> {
+                        setImageUrl(response.data.data.link);
+                        console.log(response.data.data.link)})
+                      .catch(Error => {
+                        setImageError(Error)
+                        console.log(imageError)
+                      })                         
+        },[image])
 
-
-},[])
-
+          
         const handleSubmit = (e) =>{
             e.preventDefault();
             
@@ -53,39 +60,9 @@ const Form = () => {
              courseData.append("title", title);
              courseData.append("description", description);
              courseData.append("author", author);
-             courseData.append("image", image);
+             courseData.append("image", imageUrl);
              courseData.append("video", video);
-
-             {/*axios.post('https://www.googleapis.com/upload/youtube/v3/videos', courseData)
-             .then(response => console.log(response.data))
-
- */}          
-              
-
-              {/**
-
-             axios.post('http://localhost:3004/courses', courseData);
-             axios.post('https://www.googleapis.com/upload/youtube/v3/videos', {
-               file: courseData,
-               token:this.accessToken
-             })
-               
-              axios.post('https://www.googleapis.com/upload/youtube/v3/videos?key=[AIzaSyAzkRilV1frsYInHzR41Qq1SgZWxyqdYg8]',{
-                Authorization: Bearer [642784607442-q1fl97dg1ulb5dvf125thfi01r5usbd4.apps.googleusercontent.com],
-                Accept: application/json,
-                Content-Type: application/json,
-                file: courseData
-              })
-
-
-            
- */}
-
-
-
-
-
-              }
+        }
 
 
 
@@ -93,73 +70,31 @@ const Form = () => {
   
     return ( 
         <div className="justify-center md:flex sm:items-center">
-            <form onSubmit={handleSubmit} class="container px-4 md:w-full max-w-sm">
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-        Course Title
-      </label>
-    </div>
-    <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500" id="inline-full-name" type="text" value={title} placeholder="Principles of Web Development by samuel" onChange={handleTitle} />
-    </div>
-  </div>
-
+            <form onSubmit={handleSubmit} className="container px-4 md:w-full max-w-md">
   
 
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-        Course Description
-      </label>
-    </div>
-    <div class="md:w-2/3">
-      <textarea class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500" id="inline-full-name" type="text" value={description} placeholder="Course Description" onChange={handleDescription}></textarea>
+        <InputField placeholder={"Principles of Web Development by samuel"} label={'Course Title'} value={title} type={'text'} onChange={handleTitle} />
+          
+
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline
+              -full-name">
+                Course Description
+              </label>
+           </div>
+    <div className="md:w-2/3">
+      <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-g
+      ay-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500" id="inline-full-name"
+       type="text" value={description} placeholder="Course Description" onChange={handleDescription}></textarea>
     </div>
   </div>
 
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-          Videos
-      </label>
-    </div>
-    <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500" id="inline-full-name" accept="video/mp4" type="file" onChange={handleVideo} />
-    </div>
-  </div>
-
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-        Cover Image
-      </label>
-    </div>
-    <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500" id="inline-full-name"  type="file"  onChange={handleImage} />
-    </div>
-  </div>
-
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-            Author
-          </label>
-        </div>
-        <div class="md:w-2/3">
-          <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500" id="inline-full-name" type="text" value={author} placeholder="e.g Samuel Ayegbusi" onChange={handleAuthor} />
-        </div>
-  </div>
-  
-  
-  <div class="md:flex md:items-center">
-    <div class="md:w-1/3"></div>
-    <div class="md:w-2/3">
-      <button class="shadow mb-6 bg-indigo-500 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button" onSubmit={handleSubmit}>
-        Add Content
-      </button>
-    </div>
-  </div>
+  <InputField label={'Upload video'}  onChange={handleVideo} type={'file'} accept={'video/mp4,video/x-m4v,video'}  />
+  <InputField label={'Course Cover Image'}  onChange={handleImage} type={'file'} accept={'image/*'}  />
+  <InputField label={'Author'} placeholder={'e.g Samuel Ayegbusi'}  onChange={handleAuthor} type={'text'}  />
+  <Button onSubmit={handleSubmit} />
+ 
 </form>
         </div>
      );
