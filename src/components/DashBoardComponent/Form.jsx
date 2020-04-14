5745e52b3920b6cf457ab769b9f6417544f7cd91
Sambalicious <stemitope370@gmail.com>
2020-04-14 15:33:02 +0100

    added transition to the cards

--- src/components/DashBoardComponent/Form.jsx
+++ src/components/DashBoardComponent/Form.jsx
@@ -38,7 +38,7 @@ const Form = () => {
         const [imageError, setImageError] = useState('');
 
 
-        {/**
+        
         useEffect(()=>{            
           axios.post("https://api.imgur.com/3/image",image, {
                         headers:{
@@ -52,22 +52,23 @@ const Form = () => {
                         console.log(imageError)
                       })                         
         },[image]);
-         */}
+         
 
         useEffect(()=>{
-          axios.post("https://www.googleapis.com/youtube/v3/videos?part=snippet&autoLevels=true&notifySubscribers=true&key=AIzaSyAzkRilV1frsYInHzR41Qq1SgZWxyqdYg8",
-              {
-                Authorization: "Bearer 642784607442-q1fl97dg1ulb5dvf125thfi01r5usbd4.apps.googleusercontent.com",
-                Accept: "application/json",
+          axios.post("https://www.googleapis.com/youtube/v3/videos",
+                     video, {
+                  headers: {
+                "Authorization": "Bearer ya29.a0Ae4lvC2R6TUz1hR8juqDbR8dwHB3GNuyMTBCgVLvIm2Ef3GNcT-Ovxuh0tJrMvkfv4yGMAsiJNQiVW2LYsmLjvaW-hXY9ufzxBTHJ0AVMU_plcOVM3cmgMvDh4raX7ppPnnrMq9a67K52jHf4b7wM7LpHNb4ZI3zi-Q",
+                "Accept": "application/json",
                 "Content-Type": "application/json"
-              }    
-
-              ).then(response=>{
+              }}).then(response=>{
                 console.log(response)
               }).catch(error =>{
                 console.log(error)
               })
         })
+
+        {/**" */}
 
           
         const handleSubmit = (e) =>{
@@ -88,22 +89,21 @@ const Form = () => {
   
     return ( 
         <div className="justify-center md:flex sm:items-center">
-            <form onSubmit={handleSubmit} className="container px-4 md:w-full max-w-md">
+            <form onSubmit={handleSubmit} className="container max-w-md px-4 md:w-full">
   
 
         <InputField placeholder={"Principles of Web Development by samuel"} label={'Course Title'} value={title} type={'text'} onChange={handleTitle} />
           
 
-          <div className="md:flex md:items-center mb-6">
+          <div className="mb-6 md:flex md:items-center">
             <div className="md:w-1/3">
-              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline
+              <label className="block pr-4 mb-1 font-bold text-gray-500 md:text-right md:mb-0" htmlFor="inline
               -full-name">
                 Course Description
               </label>
            </div>
     <div className="md:w-2/3">
-      <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-g
-      ay-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500" id="inline-full-name"
+      <textarea className="w-full px-4 py-2 leading-tight bg-gray-200 border-2 border-gray-200 rounded appearance-none text-g ay-700 focus:outline-none focus:bg-white focus:border-indigo-500" id="inline-full-name"
        type="text" value={description} placeholder="Course Description" onChange={handleDescription}></textarea>
     </div>
   </div>
--- src/components/LandingPage/OfferComponent.jsx
+++ src/components/LandingPage/OfferComponent.jsx
@@ -9,13 +9,13 @@ const OfferComponent = ({pics, title, description, author}) => {
 
             <div className="justify-center mx-2 mb-4 sm:container md:shadow-xl sm:shadow-md md:flex">
 
-                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg  transition duration-500 ease-in-out  transform hover:-translate-y-1 hover:scale-110">
                         <img class="w-full" src={pics} alt="Sunset in the mountains"/>
                     <div class="px-6 py-4">
                         
                     <div class="font-bold text-xl mb-2">{title}
                    <div>
-                       <p className="inline-block  rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Author: {author} </p>
+                       <p className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 rounded-full">Author: {author} </p>
                    </div>
                     </div>
                         <p class="text-gray-700 text-base">
